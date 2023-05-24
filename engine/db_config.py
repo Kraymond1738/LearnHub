505f152bf291599c2a569d6baa4f81ce935b3002
Kasumba <kraymond1738@gmail.com>
2023-05-24 16:08:34 +0000

    Secon

--- engine/db_config.py
+++ engine/db_config.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
-#configure the database
+"""
+configure the database
+replace "Kasumba" with your user name and "Kraymond12"
+with your password for your mysql database
+"""
+
 
 SQLALCHEMY_DATABASE_URI = 'mysql://Kasumba:Kraymond12@localhost/LearnHub'
--- engine/db_mod.py
+++ engine/db_mod.py
@@ -6,17 +6,13 @@ from engine.database import db
 from engine.database import User
 
 
-# Functions for database operations on each table
+"""
+Fnctions for database operations on each table
+"""
 
 def update_user(user_id):
     """
     Update a user in the database.
-
-    Args:
-        user_id (int): The ID of the user to update.
-
-    Returns:
-        None
     """
     user = User.query.get(user_id)
     if user:
@@ -24,13 +20,7 @@ def update_user(user_id):
 
 def delete_user(user_id):
     """
-    Delete a user from the database.
-
-    Args:
-        user_id (int): The ID of the user to delete.
-
-    Returns:
-        None
+    user_id (int): The ID of the user to delete.
     """
     user = User.query.get(user_id)
     if user:
@@ -40,27 +30,14 @@ def delete_user(user_id):
 def add_user(user):
     """
     Add a new user to the database.
-
-    Args:
-        user (User): The user object to add.
-
-    Returns:
-        None
     """
     db.session.add(user)
     db.session.commit()
 
-# Rest of the functions with docstrings
 
 def update_video_meeting(video_meeting_id):
     """
     Update a video meeting in the database.
-
-    Args:
-        video_meeting_id (int): The ID of the video meeting to update.
-
-    Returns:
-        None
     """
     video_meeting = VideoMeeting.query.get(video_meeting_id)
     if video_meeting:
@@ -69,12 +46,6 @@ def update_video_meeting(video_meeting_id):
 def delete_video_meeting(video_meeting_id):
     """
     Delete a video meeting from the database.
-
-    Args:
-        video_meeting_id (int): The ID of the video meeting to delete.
-
-    Returns:
-        None
     """
     video_meeting = VideoMeeting.query.get(video_meeting_id)
     if video_meeting:
@@ -84,12 +55,6 @@ def delete_video_meeting(video_meeting_id):
 def add_video_meeting(video_meeting):
     """
     Add a new video meeting to the database.
-
-    Args:
-        video_meeting (VideoMeeting): The video meeting object to add.
-
-    Returns:
-        None
     """
     db.session.add(video_meeting)
     db.session.commit()
