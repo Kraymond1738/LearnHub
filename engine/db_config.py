1883f09ebc9e8c71e9709694c3608ddf8b4dddb1
Kasumba Raymond <kasumbaraymond0756519001@gmail.com>
2023-06-01 13:54:04 +0300

    config file

--- engine/db_config.py
+++ engine/db_config.py
@@ -3,7 +3,9 @@
 """
 configure the database
 replace "Kasumba" with your user name and "Kraymond12"
-with your password for your mysql database
+with your password for your mysql database to connect to your database
+Dont forget to first create the database "LearnHub"
+checkout the "create_database.sql" file
 """
 
 
